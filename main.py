a4ff63dcead6c1c6fe702155dd576281f0135dda
Adm <khost@bk.ru>
2023-06-19 00:00:58 +0300

    - 1. Creating new user.+ - 2. Create other tables to db. - 3. List all existing users. - 4. Autorisation. - 5. Search chat companion. - 6. Check dialog existing. - 7. Load dialog (history). - 8. Send message. - 9. Save dialog to db.

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 # from flask import Flask
 import socket
+import sqlite3
 
 class MyServer():
     def __init__(self):
         self.action_dict = {'main_data': self.main_data_handler,
                             'users': self.users_handler,
+                            'new_user': self.new_user_handler,
                             'dialog': self.dialog_handler,
                             'new_msg': self.new_msg_handler
+
 
                             }
 
         # starting server
+        self.sqlite = SQLite()
         self.s_srv = socket.create_server(('127.0.0.1', 1234))
         self.s_srv.listen(100)
         while True:
+
             print('working..')
             self.client_socket, address = self.s_srv.accept()
-            self.client_socket.send(bytes("Добро пожаловать на сервер!!!", 'utf-8'))
-            self.req_data = self.client_socket.recv(1024).decode('utf-8')
-            print(self.req_data)
-            #self.client_socket.send(bytes(self.req_data, 'utf-8'))
-            self.action_dict[self.req_data]()
-            # self.client_socket.sendmsg()
+            with self.client_socket:
+                self.client_socket.send(bytes("Добро пожаловать на сервер!!!", 'utf-8'))
+                self.req_data = self.client_socket.recv(1024).decode('utf-8')
+                print(self.req_data)
+                #self.client_socket.send(bytes(self.req_data, 'utf-8'))
+                self.action_dict[self.req_data.split()[0]]()
+                # self.client_socket.sendmsg()
 
 
 
@@ -33,6 +39,11 @@ class MyServer():
         content = 'USERS FROM SERVER!!!!!!!!'.encode('utf-8')
         self.client_socket.send(content)
 
+    def new_user_handler(self):
+        answer = self.sqlite.create_user(*self.req_data.split()[1:])
+        self.client_socket.send(answer.encode('utf-8'))
+
+
     def dialog_handler(self):
         content = 'dialog FROM SERVER!!!!!!!!'.encode('utf-8')
         self.client_socket.send(content)
@@ -40,6 +51,30 @@ class MyServer():
     def new_msg_handler(self):
         content = 'new msg FROM SERVER!!!!!!!!'.encode('utf-8')
         self.client_socket.send(content)
+
+class SQLite():
+    def __init__(self):
+        self.db = sqlite3.connect('chatapp.db')
+        self.curs = self.db.cursor()
+
+        self.curs.execute('''CREATE TABLE IF NOT EXISTS users (
+        login TEXT,
+        pass TEXT,
+        gender INT 
+        )''')
+        self.db.commit()
+
+    def create_user(self, username, password, gender):
+        self.curs.execute('''SELECT login FROM users''')
+        if self.curs.fetchone() is None:
+            self.curs.execute(f"INSERT INTO users VALUES (?, ?, ?)", (username, password, gender))
+            self.db.commit()
+            return ("User created!")
+        else:
+            return ("Error! User already exists!")
+
+
+
 
 
 
